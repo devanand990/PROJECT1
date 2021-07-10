8d1fd2d1a64c0ccf1d896ef2d2eb226e873838b8
devanand990 <87178567+devanand990@users.noreply.github.com>
2021-07-10 13:59:20 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+var square
+
+
+
+
+
+
+
+
+
+function setup() { square = createSprite(168,200,50,50)
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+  background(30);
+if(keyIsDown(RIGHT_ARROW)){background("red")}
+if(keyIsDown(LEFT_ARROW)){background("yellow")}
+if(keyIsDown(DOWN_ARROW)){background("green")}
+if(keyIsDown(UP_ARROW)){background("pink")  }
+
+
+
+
+
+
+
+
+drawSprites()}
+
+
+
+
